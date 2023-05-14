06eca4b6da74c92e0369c2307a341bad01f5a94b
Dio.Ye <diov87@outlook.com>
2023-05-14 18:27:46 +0800

    feature: Bundle extensions & add logo

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ## FoxList: Your Foxy Reading Companion
 
 A Firefox sidebar reading list. Inspired by Brave Browser.
+
+### Builing
+
+1. Make sure you have Node and NPM installed
+2. Download/clone this repo
+3. Run the build command below:
+    ```
+    pnpm i
+    pnpm build
+    ```
+4. The built extension will be located in the `web-ext-artifacts` directory.
--- plugin/manifest.ts
+++ plugin/manifest.ts
@@ -3,12 +3,26 @@ import { Manifest } from 'webextension-polyfill'
 const manifest: Manifest.WebExtensionManifest = {
   manifest_version: 3,
   name: '__MSG_extName__',
+  description: '__MSG_extDesc__',
   version: '0.0.1',
+  author: 'Dio.Ye',
   default_locale: 'en',
   permissions: ['storage', 'tabs'],
+  icons: {
+    16: 'icons/icon16.png',
+    32: 'icons/icon32.png',
+    48: 'icons/icon48.png',
+    128: 'icons/icon128.png'
+  },
   sidebar_action: {
+    default_title: '__MSG_extName__',
     default_panel: 'sidebar.html',
-    default_title: '__MSG_extName__'
+    default_icon: {
+      16: 'icons/icon16.png',
+      32: 'icons/icon32.png',
+      48: 'icons/icon48.png',
+      128: 'icons/icon128.png'
+    },
   },
   commands: {
     _execute_sidebar_action: {
